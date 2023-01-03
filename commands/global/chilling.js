0d0038dc14b8ae52bc880d1eca64a0edf66e9666
Dawid <dawid_go@outlook.com>
2023-01-03 12:58:20 +0100

    Update chilling.js
    Deferred the reply, so on slower hardware the bot can still response in time.

--- commands/global/chilling.js
+++ commands/global/chilling.js
@@ -7,12 +7,13 @@ module.exports = {
 		.setDescription('Wanna see me chilling? Try this then.'),
 
 	async execute(interaction) {
+		await interaction.deferReply();
 		const interactionembed = new EmbedBuilder()
 			.setTitle('Spining around...')
 			.setImage('attachment://sealspin.gif')
 			.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 			.setColor('#00ffff');
 
-		interaction.reply({ embeds: [interactionembed], files: ['./resources/sealspin.gif'] });
+		interaction.editReply({ embeds: [interactionembed], files: ['./resources/sealspin.gif'] });
 	},
 };
